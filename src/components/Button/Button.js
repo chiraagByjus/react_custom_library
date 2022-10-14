57d2a7e373112cbd3ba3011fc2b54b3d954341ae
Chiraag A <chiraag.a01@byjus.com>
2022-10-14 16:46:27 +0530

    error fix

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,11 @@ import "./Button.css";
 
 const Button = (props) => {
   const { variant, children, ...rest } = props;
-  return <button className={`button ${variant}`}>{children}</button>;
+  return (
+    <button className={`button ${variant}`} {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
